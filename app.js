8cd8c74d614cc01ebd159badce53dfcd3737832e
Amkam <amaduskamara@gmail.com>
2022-06-17 10:07:56 +0000

    Setup env files and logger middleware for logging requests

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require("express");
+require("dotenv").config();
 
 // Express app
 const app = express();
 
+// Middlewares
+// Passing req body
+app.use(express.json());
+
+// Logging req url and method
+app.use((req, res, next) => {
+  console.log(req.url, req.method);
+  next();
+});
+
+// Routes
+app.get("/", (req, res) => {
+  res.json({ msg: "Welcome to Workout App" });
+});
+
 // Listen for request
-app.listen(4000, () => {
-  console.log("App running on port: 4000");
+app.listen(process.env.PORT, () => {
+  console.log(`App running on port: ${process.env.PORT}`);
 });
